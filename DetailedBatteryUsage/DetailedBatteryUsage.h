3420a452c74ad0825d183af82465f7fafb133ae3
Hamza Sood <hamzasood@Hamzas-MacBook-Pro.local>
2014-10-30 17:03:01 +0000

    Initial Commit

---
+++ DetailedBatteryUsage/DetailedBatteryUsage.h
@@ -0,0 +1,13 @@
+//
+//  DetailedBatteryUsage.h
+//  DetailedBatteryUsage
+//
+//  Created by Hamza Sood on 30/10/2014.
+//  Copyright (c) 2014 Hamza Sood. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DetailedBatteryUsage : NSObject
+
+@end
---
+++ DetailedBatteryUsage/DetailedBatteryUsage.m
@@ -0,0 +1,13 @@
+//
+//  DetailedBatteryUsage.m
+//  DetailedBatteryUsage
+//
+//  Created by Hamza Sood on 30/10/2014.
+//  Copyright (c) 2014 Hamza Sood. All rights reserved.
+//
+
+#import "DetailedBatteryUsage.h"
+
+@implementation DetailedBatteryUsage
+
+@end
